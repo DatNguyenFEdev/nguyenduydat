850930c4b08021d3d5247b6ac2bda1a1afbc6920
DatNguyen <duydat10122001@gmail.com>
2022-04-12 15:50:08 +0700

    use ethersjs

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div class="container">
       <h1 class="title">2nd largest of Transactions:</h1>
-      <span class="sub-title">1.10 ETH</span>
+      <span class="sub-title">{{SecondLargestTransaction.toFixed(2)}} ETH</span>
       <div class="total-list">
         <div class="total">
           <div class="total-top">
@@ -11,7 +11,7 @@
             </div>
             <h5 class="yellow">Total transactions:</h5>
           </div>
-          <strong>79</strong>
+          <strong>{{ totalTransactions }}</strong>
         </div>
         <div class="total">
           <div class="total-top">
@@ -20,7 +20,7 @@
             </div>
             <h5 class="blue">AVG of block time</h5>
           </div>
-          <strong>19.455</strong>
+          <strong>{{ timeAVG }}</strong>
         </div>
         <div class="total">
           <div class="total-top">
@@ -29,7 +29,7 @@
             </div>
             <h5 class="green">AVG of ETH/transactions</h5>
           </div>
-          <strong>1.10 ETH</strong>
+          <strong>{{ totalEther.toFixed(2) }} ETH</strong>
         </div>
       </div>
 
@@ -107,10 +107,17 @@
 
 <script>
 import axios from "axios";
+// import Web3 from "web3";
+import { ethers } from "ethers";
+// import moment from "moment"
 export default {
   name: "App",
   components: {},
-  created() {
+  async created() {
+    await this.getBlock();
+    await this.handleTimeAVG();
+    await this.handleTransactionAVG();
+    await this.handleSecondLargestTransactionAVG()
     axios
       .post(this.baseURL, {
         page: 1,
@@ -125,6 +132,9 @@ export default {
         this.cards = data.data.data.fundProjects;
       });
   },
+  beforeMount() {
+
+  },
   data() {
     return {
       baseURL: "http://139.99.62.190:8000/api/v1/fund_projects/filter",
@@ -135,32 +145,19 @@ export default {
       status: "",
       totalRaise: "",
       personalAllocation: "",
+      transactions: [],
+      totalTransactions: 0,
+      timeAVG: 0,
+      totalEther: 0,
+      SecondLargestTransaction:0
     };
   },
-  // mounted() {
-  //   window.addEventListener('scroll',()=> {
-  //     let scrollTop = document.documentElement.scrollTop;
-  //     let scrollHeight = document.documentElement.scrollHeight;
-  //     let clientHeight = document.documentElement.clientHeight;
-  //   })
-  //   let vm = this;
-  //   axios.post(this.baseURL, {
-  //       page: 1,
-  //       pageSize: 20,
-  //       symbol: "",
-  //       name: "",
-  //       status: "SOLD_OUT",
-  //       totalRaise: [100, 200],
-  //       personalAllocation: [0.07, 0.08],
-  //     })
-  // },
   methods: {
     handleLoadMore() {
       if (this.length > this.listCard.length) return;
       this.length = this.length + 3;
     },
     handleFilter() {
-      console.log(this.name);
       axios
         .post(this.baseURL, {
           name: this.name,
@@ -173,6 +170,114 @@ export default {
           console.log("success", data);
         });
     },
+    // ==============================ETHERS-JS============================
+    async getBlock() {
+      const PROVIDER = "https://rinkeby.infura.io/v3/e5b97339938341618b45e7e0d7e7d225"
+      const provider = new ethers.providers.JsonRpcProvider(PROVIDER)
+      const fromBlock = 10441830;
+      const toBlock = 10441840;
+      var totalTransaction = 0;
+      for (let block = fromBlock; block <= toBlock; block++) {
+       let value = await provider.getBlockWithTransactions(block)
+       this.transactions.push(value)
+       totalTransaction += value.transactions.length;
+      }
+      this.totalTransactions = totalTransaction;
+      // console.log(this.totalTransactions);
+      // console.log('oop',this.transactions);
+    },
+    // ==========================AVG TIME.======================
+    handleTimeAVG() {
+      const time = this.transactions.map(
+        (transaction) => transaction.timestamp
+      );
+      let sumTime = 0;
+      for (let i = 0; i < time.length; i++) {
+        sumTime += time[i];
+      }
+      let total = (sumTime / time.length).toString().slice(0, 5);
+      let convertTotal = total.replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+      return (this.timeAVG = convertTotal);
+    },
+    // ======================handle avg transaction in transactions==================
+    handleTransactionAVG() {
+      const transaction = this.transactions.map(transaction => transaction.transactions);
+      let array = []
+      let total =0
+      let totalAVG = 0
+      for(let i=0;i<transaction.length;i++) {
+        transaction[i].map(item=> {
+          total += ((parseInt(item.value)/Math.pow(10,18))/transaction.length)
+      })
+      array.push(total)
+      totalAVG = array.reduce((previousValue, currentValue) => {
+        return previousValue + currentValue
+      },0)
+      }
+      this.totalEther = totalAVG /transaction.length
+    },
+    // ======================handle avg transaction second largest in transactions==================
+    handleSecondLargestTransactionAVG() {
+      let transaction = this.transactions.map(transaction => transaction.transactions);
+      // let array = []
+      // let total =0
+        let listEther = []
+      for(let i=0;i<transaction.length;i++) {
+        transaction[i].map(item=> {
+          listEther.push(parseInt(item.value)/Math.pow(10,18))
+      })
+      }
+      listEther.sort(function(a, b) {
+        return a - b;
+      });
+      this.SecondLargestTransaction = listEther[listEther.length-2]
+    }
+    // ==============================WEB3-JS==============================
+    // async getBlock() {
+    //   const PROVIDER ="https://rinkeby.infura.io/v3/e5b97339938341618b45e7e0d7e7d225";
+    //   const web3 = new Web3(new Web3.providers.HttpProvider(PROVIDER));
+    //   const fromBlock = 10441830;
+    //   const toBlock = 10441840;
+    //   var totalTransaction = 0;
+
+    //   for (let block = fromBlock; block <= toBlock; block++) {
+    //     let blockData = await web3.eth.getBlock(block, true);
+    //     this.transactions.push(blockData);
+    //     totalTransaction += blockData.transactions.length;
+    //   }
+    //   this.totalTransactions = totalTransaction;
+
+    //   // ==========================trung bình số ether trên 1 transaction.====================
+    //   const transaction = this.transactions.map(
+    //     (transaction) => transaction.transactions
+    //   );
+    //   const totalAVG = transaction.map((item) => item);
+    //   let total;
+    //   for (let i = 0; i <= transaction.length; i++) {
+    //     for (let j = 0; j <= totalAVG.length; j++) {
+    //       total = totalAVG[j].reduce((previousValue, currentValue) => {
+    //         return (
+    //           previousValue + parseInt(currentValue.value) / Math.pow(10, 18)
+    //         );
+    //       }, 0);
+    //       this.totalEther = total;
+    //       return total;
+    //     }
+    //   }
+    // },
+    // ==========================TIME.======================
+    // handleTimeAVG() {
+    //   const time = this.transactions.map(
+    //     (transaction) => transaction.timestamp
+    //   );
+    //   let sumTime = 0;
+    //   for (let i = 0; i < time.length; i++) {
+    //     sumTime += time[i];
+    //   }
+    //   let total = (sumTime / time.length).toString().slice(0, 5);
+    //   let convertTotal = total.replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
+    //   return (this.timeAVG = convertTotal);
+    // },
   },
   computed: {
     listCard() {
@@ -216,7 +321,7 @@ export default {
 }
 .green {
   color: #1f8b24;
-  }
+}
 .blue {
   color: #31b4d9;
 }
